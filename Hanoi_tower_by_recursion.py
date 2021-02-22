2853e507b2308ffb0225455934e5903560d7bd84
Alireza <63372101+Alireza-rahnama@users.noreply.github.com>
2021-02-22 19:56:53 -0330

    Update Hanoi_tower_by_recursion.py

--- Hanoi_tower_by_recursion.py
+++ Hanoi_tower_by_recursion.py
@@ -1,7 +1,8 @@
 import sys
 
 def hanoi(n, left):
-    if n == 0: return " "
+    if n == 0:
+        return " "
     move = str(n)
     move += "L" if left else "R"
     return hanoi(n-1, not(left)) + move + hanoi(n-1, not(left))
